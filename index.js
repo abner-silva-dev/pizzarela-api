b9fb8c8ea843d133456ad90d4f0e934a6c5d7462
abner-silva-dev <dylanabner14@gmail.com>
2024-04-24 17:26:21 -0600

    Vercel deploy v3

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+const port = process.env.PORT || 3000;
+
 const app = require('./app');
 
 app.use('/', (req, res) => {
   res.end('Server listen');
 });
 
-app.listen(process.env.PORT, () => {
-  console.log('Server listen in port 3000');
+app.listen(port, () => {
+  console.log(`Server listen in port ${port}`);
 });
